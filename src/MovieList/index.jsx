f06e300807c48825a7b277b43e503c9eea9ed9f9
wonhyejong <zukommen@gmail.com>
2022-06-09 12:28:28 +0900

    movie add2

--- src/MovieList/index.jsx
+++ src/MovieList/index.jsx
@@ -4,16 +4,23 @@ import movieData from './../data/movieData.json';
 
 const MovieList = () => {
     const movieList = movieData;
+    const gradeColor = {
+        '12':'gradeinfo on12', 
+        '15':'gradeinfo on15' ,
+        '전체':'gradeinfo onall' ,
+    }
+
     return (
         <div>
             <ul className='movie-list'>
                 {
                     movieList.map((movie)=>{
+                        const gradeValue=movie.grade;
                         return(
                             <li>
                                 <div className='imgbox'>
                                     <img src={movie.poster} alt={movie.title} />
-                                    <span className='gradeinfo'>
+                                    <span className={gradeColor[gradeValue]}>
                                         {movie.grade}
                                     </span>
                                     <div className="dimmed">
@@ -35,23 +42,7 @@ const MovieList = () => {
 
                 }
 
-              {/*   <li>
-                    <div className='imgbox'>
-                        <img src="" alt="" />
-                        <span className='gradeinfo'>12</span>
-                        <div className="dimmed">
-                          <a href="#"> 예매하기 </a>
-                          <a href="#"> 상세정보 </a>
-                        </div>
-                    </div>
-                    <div className='txtbox'>
-                        <div className='title'>제목</div>
-                        <div className='info'>
-                            <span>예매율 <strong className='rateinfo'>0.1</strong> %</span>
-                            <span>★ <strong className='starinfo'>8.9</strong></span>
-                        </div>
-                    </div>
-                </li> */}
+             
             </ul>
 
         </div>
